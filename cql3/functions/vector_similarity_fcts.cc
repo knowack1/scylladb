d021cb84c150d48ed1258e51c9daea833c56c5eb
Dawid Pawlik <dawid.pawlik@scylladb.com>
2025-12-11 15:10:51 +0100

    vector_similarity_fcts: add calculating similarity between vectors
    This commit introduces a `compute_similarity` function overriden
    by each similarity function using their expected metric to calculate
    the similarity between given vectors.
    
    The implementation of those calculations is inspired by:
    https://github.com/unum-cloud/USearch/blob/a2f17599101729d667dc0260dd278852d9098183/include/usearch/index_plugins.hpp#L1304-L1385
    but changed to return the similarity instead of the distance to match
    Cassandra's implementation.
    
    The similarity metrics shows how similar the vectors are
    in range from 0 - not similar at all to 1 - the same.

--- cql3/functions/vector_similarity_fcts.cc
+++ cql3/functions/vector_similarity_fcts.cc
@@ -17,6 +17,55 @@ namespace functions {
 
 namespace {
 
+float compute_cosine_similarity(const std::vector<data_value>& v1, const std::vector<data_value>& v2) {
+    double dot_product = 0.0;
+    double norm_a = 0.0;
+    double norm_b = 0.0;
+
+    for (size_t i = 0; i < v1.size(); ++i) {
+        double a = value_cast<float>(v1[i]);
+        double b = value_cast<float>(v2[i]);
+
+        dot_product += a * b;
+        norm_a += a * a;
+        norm_b += b * b;
+    }
+
+    if (norm_a == 0 && norm_b == 0) {
+        return 0;
+    }
+    if (norm_a == 0 || norm_b == 0) {
+        return 1;
+    }
+    return (1 + (dot_product / (std::sqrt(norm_a) * std::sqrt(norm_b)))) / 2;
+}
+
+float compute_euclidean_similarity(const std::vector<data_value>& v1, const std::vector<data_value>& v2) {
+    double sum = 0.0;
+
+    for (size_t i = 0; i < v1.size(); ++i) {
+        double a = value_cast<float>(v1[i]);
+        double b = value_cast<float>(v2[i]);
+
+        double diff = a - b;
+        sum += diff * diff;
+    }
+
+    return (1 / (1 + sum));
+}
+
+float compute_dot_product_similarity(const std::vector<data_value>& v1, const std::vector<data_value>& v2) {
+    double dot_product = 0.0;
+
+    for (size_t i = 0; i < v1.size(); ++i) {
+        double a = value_cast<float>(v1[i]);
+        double b = value_cast<float>(v2[i]);
+        dot_product += a * b;
+    }
+
+    return ((1 + dot_product) / 2);
+}
+
 void validate_vector_type(const function_name& name, const data_type& type, const shared_ptr<assignment_testable>& arg, const data_dictionary::database& db) {
     if (!type->is_vector()) {
         throw exceptions::invalid_request_exception(fmt::format("Function {} requires a float vector argument, but found {} of type {}", name,
@@ -70,10 +119,44 @@ bytes_opt vector_similarity_fct::execute(std::span<const bytes_opt> parameters) 
     if (std::any_of(parameters.begin(), parameters.end(), [](const auto& param) {
             return !param;
         })) {
-        throw exceptions::invalid_request_exception("Vector similarity functions cannot be executed with null arguments");
+        return std::nullopt;
     }
 
-    return std::nullopt; // Unimplemented
+    const auto& type = arg_types()[0];
+    data_value v1 = type->deserialize(*parameters[0]);
+    data_value v2 = type->deserialize(*parameters[1]);
+
+    auto get_vector_elements = [](const data_value& dv) -> const std::vector<data_value>& {
+        return value_cast<std::vector<data_value>>(dv);
+    };
+
+    const auto& v1_elements = get_vector_elements(v1);
+    const auto& v2_elements = get_vector_elements(v2);
+
+    if (v1_elements.size() != v2_elements.size()) {
+        throw exceptions::invalid_request_exception(
+                fmt::format("All arguments must have the same vector dimensions, but found vector<float, {}> and vector<float, {}>", v1_elements.size(),
+                        v2_elements.size()));
+    }
+
+    using similarity_function_t = std::function<float(const std::vector<data_value>&, const std::vector<data_value>&)>;
+    static const std::unordered_map<function_name, similarity_function_t> SIMILARITY_FUNCTIONS = {
+            {SIMILARITY_COSINE_FUNCTION_NAME,
+                    [](const std::vector<data_value>& v1_elements, const std::vector<data_value>& v2_elements) -> float {
+                    return compute_cosine_similarity(v1_elements, v2_elements);
+                    }},
+            {SIMILARITY_EUCLIDEAN_FUNCTION_NAME,
+                    [](const std::vector<data_value>& v1_elements, const std::vector<data_value>& v2_elements) -> float {
+                    return compute_euclidean_similarity(v1_elements, v2_elements);
+                    }},
+            {SIMILARITY_DOT_PRODUCT_FUNCTION_NAME,
+                    [](const std::vector<data_value>& v1_elements, const std::vector<data_value>& v2_elements) -> float {
+                    return compute_dot_product_similarity(v1_elements, v2_elements);
+                    }},
+    };
+
+    float result = SIMILARITY_FUNCTIONS.at(_name)(v1_elements, v2_elements);
+    return float_type->decompose(result);
 }
 
 } // namespace functions
